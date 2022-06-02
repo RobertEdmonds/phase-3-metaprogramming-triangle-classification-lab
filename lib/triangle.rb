d0dd9e92a8b556adf0a8fc642af73d6280444fb2
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-06-02 13:26:15 -0600

    commit tree 7a462b762603b868c3c312d6e4d92e40e56495c7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
   # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize (side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  def kind
+    validate_triangle
+    if side1 == side2 && side2 == side3 && side1 == side3
+      :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  def validate_triangle
+    happy = [side1, side2, side3].all? {|side| side > 0}
+    if happy == false
+      raise TriangleError
+    elsif side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1
+      raise TriangleError
+    end
+  end
+  class TriangleError < StandardError
+
+  end
+
 end
